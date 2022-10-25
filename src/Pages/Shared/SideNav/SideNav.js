c2cf61d0593f4436fd478674271446a653adb8e9
mostakshaikh50 <mostak.50.cse@gmail.com>
2022-10-25 22:22:44 +0100

    data creation and load

--- src/Pages/Shared/SideNav/SideNav.js
+++ src/Pages/Shared/SideNav/SideNav.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const SideNav = () => {
+    const [courses, setCourses] = useState([]);
+
+    useEffect( ()=>{
+        fetch('http://localhost:5000/courses-categories')
+        .then(res => res.json())
+        .then(data => setCourses(data))
+    })
     return (
         <div>
-            <h2>Side Nav Component</h2>
+            <h4>Our All Courses: {courses.length}</h4>
+            {
+                courses.map(course => <p
+                  key={course.id}
+
+                ><Link to={`/course/${course.id}`}>{course.name}</Link></p>)
+            }
         </div>
     );
 };
--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -1,12 +1,26 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../layout/Main";
+import Course from "../../Pages/Course/Course/Course";
+import CourseDetails from "../../Pages/CourseDetails/CourseDetails/CourseDetails";
+import Home from "../../Pages/Home/Home/Home";
 
 export const routes = createBrowserRouter([
     {
         path:'/',
         element: <Main></Main>,
         children: [
-            
+         {
+            path: '/',
+            element: <Home></Home>
+         },
+         {
+            path: '/course/:id',
+            element: <Course></Course>
+         },
+         {
+            path: 'coursedetails/:id',
+            element: <CourseDetails></CourseDetails>
+         }
         ]
     }
 ])
